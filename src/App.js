2a2182e852a80b56c935da4a3e064eb5a820aa4a
Diana-nyamai <dnyamai.dn@gmail.com>
2020-10-08 15:25:40 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,38 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react'
 import './App.css';
- 
-const Number =({numbers}) =>(
-      <li>
-        <div className="text" contentEditable="true" spellCheck="false">
-                {numbers}
-        </div>
-      </li>
-  
-);
+import Numbers from './Numbers';
 
-  
+export class App extends Component {
+  state ={
+    name:"circle"
+  }
+  startRotation = () =>{
+    this.setState({
+        name:"circle start-rotate"
+    });
 
-const App= () => {
-  return (
-    <div className="App">
-      <div className="arrow"></div>
-      <ul className="circle">
-         <Number numbers="1"/>
-         <Number numbers="2"/>
-         <Number numbers="3"/>
-         <Number numbers="4"/>
-         <Number numbers="5"/>
-         <Number numbers="6"/>
-         <Number numbers="7"/>
-         <Number numbers="8"/>
-         <Number numbers="9"/>
-         <Number numbers="10"/>
-         <Number numbers="11"/>
-         <Number numbers="12"/>
-      </ul>
-      <button className="spin-button" >SPIN</button>
-    </div>
-  );
+setTimeout(() => {
+  this.setState({
+    name:"circle start-rotate stop-rotate"
+  })
+     
+}, Math.floor(Math.random() * 10000 + 1));
 }
 
-export default App;
+  render() {
+    return (
+      <div>
+         <div className="arrow"></div>
+         <ul className={this.state.name}>
+           {
+             Numbers.numbers.map( number =>
+           <li>
+           <div className="text"  spellCheck="false">{number.no}</div>
+           </li>
+          ) }
+         </ul>
+         <button className="spin-button" onClick={this.startRotation}>Spin The Wheel</button>
+      </div>
+    )
+  }
+}
+
+export default App
